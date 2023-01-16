cf6dcc421778bfd8d87abd68b3dd1e75ef4da757
gracefullight <gracefullight.dev@gmail.com>
2023-01-16 22:45:31 +0900

    feat(plugins): ✨ channelio

---
+++ packages/docusaurus-plugin-channelio/README.md
@@ -0,0 +1,26 @@
+# @gracefullight/docusaurus-plugin-channelio
+
+This plugin provides an easy way to add channel.io to your Docusaurus documentation website.
+
+## Installation
+
+You can install this plugin via yarn:
+
+```bash
+yarn add @gracefullight/docusaurus-plugin-channelio
+```
+
+To use this plugin, you will need to configure it in your Docusaurus `docusaurus.config.js` file. You can add the following configuration to enable the plugin:
+
+```js
+module.exports = {
+  plugins: [
+    [
+      "@gracefullight/docusaurus-plugin-channelio",
+      { pluginKey: "YOUR_PLUGIN_KEY" },
+    ],
+  ],
+};
+```
+
+You need to replace "YOUR_PLUGIN_KEY" with the key you received from channel.io.
---
+++ packages/docusaurus-plugin-channelio/src/index.ts
@@ -0,0 +1,88 @@
+import type {
+  LoadContext,
+  Plugin,
+  OptionValidationContext,
+} from "@docusaurus/types";
+import { Joi } from "@docusaurus/utils-validation";
+
+// ? https://channel.io/user-chat
+export interface PluginOptions {
+  pluginKey: string;
+}
+
+export type Options = Partial<PluginOptions>;
+
+const pluginOptionsSchema = Joi.object({
+  pluginKey: Joi.string().required(),
+});
+
+export default async function channelIO(
+  _: LoadContext,
+  options: PluginOptions
+): Promise<Plugin> {
+  return {
+    name: "@gracefullight/docusaurus-plugin-channelio",
+    injectHtmlTags() {
+      return {
+        headTags: [
+          {
+            tagName: "link",
+            attributes: {
+              rel: "preconnect",
+              href: "https://cdn.channel.io",
+            },
+          },
+          {
+            tagName: "script",
+            innerHTML: `(function() {
+              var w = window;
+              if (w.ChannelIO) {
+                return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+              }
+              var ch = function() {
+                ch.c(arguments);
+              };
+              ch.q = [];
+              ch.c = function(args) {
+                ch.q.push(args);
+              };
+              w.ChannelIO = ch;
+              function l() {
+                if (w.ChannelIOInitialized) {
+                  return;
+                }
+                w.ChannelIOInitialized = true;
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.async = true;
+                s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+                s.charset = 'UTF-8';
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(s, x);
+              }
+              if (document.readyState === 'complete') {
+                l();
+              } else if (window.attachEvent) {
+                window.attachEvent('onload', l);
+              } else {
+                window.addEventListener('DOMContentLoaded', l, false);
+                window.addEventListener('load', l, false);
+              }
+            })();
+            ChannelIO('boot', {
+              "pluginKey": "${options.pluginKey}"
+            });`,
+          },
+        ],
+      };
+    },
+  };
+}
+
+export function validateOptions({
+  options,
+  validate,
+}: OptionValidationContext<Options, PluginOptions>) {
+  const validatedOptions = validate(pluginOptionsSchema, options);
+  return validatedOptions;
+}
