d5da82d7903ec0541108641adcd94ee9069f9315
gracefullight <gracefullight.dev@gmail.com>
2023-01-29 15:25:53 +0900

    feat(plugins): ✨ yandex metrica

---
+++ packages/docusaurus-plugin-yandex-metrica/README.md
@@ -0,0 +1,26 @@
+# @gracefullight/docusaurus-plugin-yandex-metrica
+
+This plugin provides an easy way to add Yandex Metrica to your Docusaurus documentation website.
+
+## Installation
+
+You can install this plugin via yarn:
+
+```bash
+yarn add @gracefullight/docusaurus-plugin-yandex-metrica
+```
+
+To use this plugin, you will need to configure it in your Docusaurus `docusaurus.config.js` file. You can add the following configuration to enable the plugin:
+
+```js
+module.exports = {
+  plugins: [
+    [
+      "@gracefullight/docusaurus-plugin-yandex-metrica",
+      { counterId: "YOUR_COUNTER_ID" },
+    ],
+  ],
+};
+```
+
+You need to replace "YOUR_COUNTER_ID" with the key you received from Yandex Metrica.
---
+++ packages/docusaurus-plugin-yandex-metrica/src/index.ts
@@ -0,0 +1,63 @@
+import type {
+  LoadContext,
+  Plugin,
+  OptionValidationContext,
+} from "@docusaurus/types";
+import { Joi } from "@docusaurus/utils-validation";
+
+export interface PluginOptions {
+  counterId: number;
+  webvisor?: boolean;
+}
+
+export type Options = Partial<PluginOptions>;
+
+const pluginOptionsSchema = Joi.object({
+  counterId: Joi.number().required(),
+  webvisor: Joi.bool(),
+});
+
+export default async function yandexMetrica(
+  _: LoadContext,
+  options: PluginOptions
+): Promise<Plugin> {
+  return {
+    name: "@gracefullight/docusaurus-plugin-yandex-metrica",
+    injectHtmlTags() {
+      return {
+        headTags: [
+          {
+            tagName: "link",
+            attributes: {
+              rel: "preconnect",
+              href: "https://mc.yandex.ru",
+            },
+          },
+          {
+            tagName: "script",
+            innerHTML: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            
+              ym(${options.counterId}, "init", {
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: ${options.webvisor ? "true" : "false"}
+              });`,
+          },
+        ],
+      };
+    },
+  };
+}
+
+export function validateOptions({
+  options,
+  validate,
+}: OptionValidationContext<Options, PluginOptions>) {
+  const validatedOptions = validate(pluginOptionsSchema, options);
+  return validatedOptions;
+}
