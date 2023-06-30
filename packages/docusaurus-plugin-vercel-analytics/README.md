7e8a527bc8fb7d4a021aa4d3404660f861b337bb
gracefullight <gracefullight.dev@gmail.com>
2023-06-30 17:15:13 +0900

    feat: ✨ vercel analytics

---
+++ packages/docusaurus-plugin-vercel-analytics/README.md
@@ -0,0 +1,19 @@
+# @gracefullight/docusaurus-plugin-vercel-analytics
+
+This plugin provides an easy way to add Vercel Analytics to your Docusaurus documentation website.
+
+## Installation
+
+You can install this plugin via pnpm:
+
+```bash
+pnpm add @gracefullight/docusaurus-plugin-vercel-analytics
+```
+
+To use this plugin, you will need to configure it in your Docusaurus `docusaurus.config.js` file. You can add the following configuration to enable the plugin:
+
+```js
+module.exports = {
+  plugins: [["@gracefullight/docusaurus-plugin-vercel-analytics"]],
+};
+```
---
+++ packages/docusaurus-plugin-vercel-analytics/src/index.ts
@@ -0,0 +1,61 @@
+import type {
+  LoadContext,
+  OptionValidationContext,
+  Plugin,
+} from "@docusaurus/types";
+import { Joi } from "@docusaurus/utils-validation";
+
+export interface PluginOptions {
+  mode: string;
+}
+
+export type Options = Partial<PluginOptions>;
+
+const pluginOptionsSchema = Joi.object({
+  mode: Joi.string(),
+});
+
+export default async function vercelAnalytics(
+  _: LoadContext,
+  options: PluginOptions
+): Promise<Plugin> {
+  return {
+    name: "@gracefullight/docusaurus-plugin-vercel-analytics",
+    injectHtmlTags() {
+      // ? https://github.com/vercel/analytics/blob/main/packages/web/src/generic.ts
+      const isDevelopment =
+        options.mode && ["development", "test"].includes(options.mode);
+      const scriptDomain = "https://va.vercel-scripts.com";
+      const scriptPath = `/v1/script${isDevelopment ? ".debug" : ""}.js`;
+
+      return {
+        headTags: [
+          {
+            tagName: "link",
+            attributes: {
+              rel: "preconnect",
+              href: scriptDomain,
+            },
+          },
+          {
+            tagName: "script",
+            attributes: {
+              src: `${scriptDomain}${scriptPath}`,
+              defer: true,
+              "data-sdkn": "@vercel/analytics",
+              "data-sdkv": "1.0.1",
+            },
+          },
+        ],
+      };
+    },
+  };
+}
+
+export function validateOptions({
+  options,
+  validate,
+}: OptionValidationContext<Options, PluginOptions>) {
+  const validatedOptions = validate(pluginOptionsSchema, options);
+  return validatedOptions;
+}
