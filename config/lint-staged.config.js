a2fe35cbbd89ab232cb40963ae3a535fc8ef4f9f
gracefullight <gracefullight.dev@gmail.com>
2024-06-22 16:16:20 +0900

    feat: ✨ oxlint

--- config/lint-staged.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  "*.{js,ts,tsx}": ["eslint --quiet --fix"],
-  "package.json": "sort-package-json",
-};
---
+++ config/lint-staged.config.mjs
@@ -0,0 +1,4 @@
+export default {
+  "**/*.{js,ts,tsx,mjs,mts}": "oxlint --fix",
+  "package.json": "sort-package-json",
+};
