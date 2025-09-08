dfecff11632cd0368ddd45fd2529af3f6c55e3c6
gracefullight <gracefullight.dev@gmail.com>
2025-09-09 00:02:15 +1000

    chore: 🔧 fix import formatting and update dependencies

--- packages/docusaurus-plugin-tailwind/src/index.ts
+++ packages/docusaurus-plugin-tailwind/src/index.ts
@@ -1,10 +1,13 @@
 import type { Plugin, PostCssOptions } from "@docusaurus/types";
 
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+
 export default function docusaurusTailwindCss(): Plugin {
   return {
     configurePostCss(postCssOptions: PostCssOptions): PostCssOptions {
-      postCssOptions.plugins.push(require("tailwindcss"));
-      postCssOptions.plugins.push(require("autoprefixer"));
+      postCssOptions.plugins.push(tailwindcss);
+      postCssOptions.plugins.push(autoprefixer);
       return postCssOptions;
     },
     name: "docusaurus-tailwindcss",
